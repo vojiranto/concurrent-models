48f95b03c5447796e1db44a1523b450d9329b928
Dmitrij Pavluk <al.lastor@bk.ru>
2018-11-15 21:28:32 +0300

    Addition catch exceptions.

--- src/Control/StateMachine/Language.hs
+++ src/Control/StateMachine/Language.hs
@@ -1,5 +1,20 @@
 {-# Language TemplateHaskell #-}
-module Control.StateMachine.Language where
+module Control.StateMachine.Language
+    ( StateMachineF(..)
+    , StateMachineL
+    , initialiseAction
+    , setFinishState
+    , addTransition
+    , addConditionalTransition
+    , staticalDo
+    , entryDo
+    , entryWithEventDo
+    , transitionDo
+    , exitWithEventDo
+    , exitDo
+    , just
+    , nothing
+    ) where
 
 import           Universum
 import           Language.Haskell.TH.MakeFunctor
@@ -52,11 +67,11 @@ entryWithEventDo
     :: (Typeable state, Typeable action)
     => state -> (action -> IO ()) -> StateMachineL ()
 entryWithEventDo newState action = liftF $ EntryWithEventDo
-    (toMachineState newState) (actionToType action) (toSafe action) id
+    (toMachineState newState) (actionToType action) (toSafeAction action) id
 
 staticalDo :: (Typeable state, Typeable event) => state -> (event -> IO ()) -> StateMachineL ()
 staticalDo currentState action = liftF $ StaticalDo
-    (toMachineState currentState) (actionToType action) (toSafe action) id
+    (toMachineState currentState) (actionToType action) (toSafeAction action) id
 
 transitionDo
     :: (Typeable state1, Typeable state2, Typeable event)
@@ -65,12 +80,12 @@ transitionDo state1 state2 action = liftF $ TransitionDo
     (toMachineState state1)
     (toMachineState state2)
     (actionToType action)
-    (toSafe action)
+    (toSafeAction action)
     id
 
 exitWithEventDo :: (Typeable state, Typeable event) => state -> (event -> IO ()) -> StateMachineL ()
 exitWithEventDo oldState action = liftF $ ExitWithEventDo
-    (toMachineState oldState) (actionToType action) (toSafe action) id
+    (toMachineState oldState) (actionToType action) (toSafeAction action) id
 
 exitDo :: Typeable state => state -> IO () -> StateMachineL ()
 exitDo oldState action = liftF $ ExitDo (toMachineState oldState) action id
@@ -81,8 +96,8 @@ just = pure . Just . toMachineState
 nothing :: IO (Maybe MachineState)
 nothing = pure Nothing
 
-toSafe :: Typeable event => (event -> IO ()) -> MachineEvent -> IO ()
-toSafe action event = catchAny (action $ fromMachineEvent event) (\_ -> pure ())
+toSafeAction :: Typeable event => (event -> IO ()) -> MachineEvent -> IO ()
+toSafeAction action event = catchAny (action $ fromMachineEvent event) (\_ -> pure ())
 
 toSafeCondition
     :: Typeable event
