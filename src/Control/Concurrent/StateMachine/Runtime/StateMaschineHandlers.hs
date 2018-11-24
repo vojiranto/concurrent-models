8a137ff61febb6ab728f80360eb6ff903e6ca3b0
Dmitrij Pavluk <al.lastor@bk.ru>
2018-11-24 23:29:58 +0300

    functions grouping

--- src/Control/Concurrent/StateMachine/Runtime/StateMaschineHandlers.hs
+++ src/Control/Concurrent/StateMachine/Runtime/StateMaschineHandlers.hs
@@ -7,6 +7,7 @@ import           Universum
 import           Data.Describe
 import           Control.Concurrent.Loger
 import           Control.Lens.At (at, Index, IxValue, At)
+import           Control.Lens.Getter (Getting)
 import           Control.Lens.TH
 import           Control.Concurrent.StateMachine.Domain
 import qualified Data.Map as M
@@ -24,35 +25,34 @@ makeLenses ''StateMaschineHandlers
 emptyHandlers :: StateMaschineHandlers
 emptyHandlers = StateMaschineHandlers mempty mempty mempty mempty mempty
 
-applyEntryDo :: Loger -> StateMaschineHandlers -> MachineState -> IO ()
-applyEntryDo toLog machineData st =
-    whenJust (machineData ^. entryDo . at st) $ \action -> do
-        toLog  $ "[entry do] " <> describe st
+applyEntryDo, applyExitDo
+    :: Loger -> StateMaschineHandlers -> MachineState -> IO ()
+
+applyEntryDo = applyState entryDo "[entry do]"
+applyExitDo  = applyState exitDo  "[exit do]"
+
+applyExitWithEventDo, applyEntryWithEventDo, applyStaticalDo
+    :: Loger -> StateMaschineHandlers -> MachineState -> MachineEvent -> IO ()
+
+applyExitWithEventDo  = applyEvent exitWithEventDo  "[exit with event do]"
+applyEntryWithEventDo = applyEvent entryWithEventDo "[entry with event do]"
+applyStaticalDo       = applyEvent staticalDo       "[statical do]"
+
+applyState
+        :: (Index m ~ MachineState, IxValue m ~ IO (), At m)
+        => Getting (Maybe (IO ())) StateMaschineHandlers m
+        -> Text -> Loger -> StateMaschineHandlers -> MachineState -> IO ()
+applyState actionLens tag toLog machineData st =
+    whenJust (machineData ^. actionLens . at st) $ \action -> do
+        toLog $ tag <> " " <> describe st
         action
 
-applyExitDo :: Loger -> StateMaschineHandlers -> MachineState -> IO ()
-applyExitDo toLog machineData st =
-    whenJust (machineData ^. exitDo . at st) $ \action -> do
-        toLog $ "[exit do] " <> describe st
-        action
-
-applyExitWithEventDo :: Loger -> StateMaschineHandlers -> MachineState -> MachineEvent -> IO ()
-applyExitWithEventDo toLog machineData st event =
-    whenJust (machineData ^.  exitWithEventDo . at2 st (eventToType event)) $ \action -> do
-        toLog $ "[exit with event do] " <> describe st <> " " <> describe event
+applyEvent
+    :: (Index m ~ (MachineState, EventType), IxValue m ~ (MachineEvent -> IO ()), At m)
+    => Getting (Maybe (MachineEvent -> IO ())) StateMaschineHandlers m
+    -> Text -> Loger -> StateMaschineHandlers -> MachineState -> MachineEvent -> IO ()
+applyEvent actionLens tag toLog machineData st event =
+    whenJust (machineData ^. actionLens . at (st, (eventToType event))) $ \action -> do
+        toLog $ tag <> " " <> describe st <> " "<> describe event
         action event
 
-applyEntryWithEventDo :: Loger -> StateMaschineHandlers -> MachineState -> MachineEvent -> IO ()
-applyEntryWithEventDo toLog machineData st event =
-    whenJust (machineData ^. entryWithEventDo . at2 st (eventToType event)) $ \action -> do
-        toLog $ "[entry with event do] " <> describe st <> " " <> describe event
-        action event
-
-applyStaticalDo :: Loger -> StateMaschineHandlers -> MachineState -> MachineEvent -> IO ()
-applyStaticalDo toLog machineData st event =
-    whenJust (machineData ^. staticalDo . at2 st (eventToType event)) $ \action -> do
-        toLog $ "[statical do] " <> describe st <> " "<> describe event
-        action event
-
-at2 :: (Index m ~ (a, b), At m) => a -> b -> Lens' m (Maybe (IxValue m))
-at2 x y = at (x, y)
