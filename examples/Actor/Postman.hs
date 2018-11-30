de007ed359c903e4fdd979b0f6cca11bd5de1225
Dmitrij Pavluk <al.lastor@bk.ru>
2018-11-30 23:20:15 +0300

    WIP postman service in actor example

--- examples/Actor/Postman.hs
+++ examples/Actor/Postman.hs
@@ -3,9 +3,10 @@ module Actor.Postman where
 import           Universum
 import           Data.Dynamic
 import qualified Data.Map                                                   as M
+--import           Data.IORef
 
---import           Data.Flag        -- To report about successful completion.
---import           Control.Concurrent.Loger
+import           Data.Flag        -- To report about successful completion.
+import           Control.Concurrent.Loger
 import           Control.Concurrent.Actor
 
 newtype Notify = Notify Dynamic
@@ -13,58 +14,64 @@ newtype Notify = Notify Dynamic
 packNotify :: Typeable a => (a -> IO ()) -> Notify
 packNotify = Notify . toDyn
 
-
 newtype Subscribers = Subscribers (M.Map EventType (M.Map TextId Notify))
-
-broadcast' :: Typeable msg => Subscribers -> msg -> IO ()
-broadcast' (Subscribers subscribers) msg =
-    whenJust (rawDataToType msg `M.lookup` subscribers) $
-        \broadcastList -> forM_ (M.elems broadcastList) $
-            \(Notify dyn) -> whenJust (fromDynamic dyn) ($ msg)
-
-emptySubscribers :: Subscribers
-emptySubscribers = Subscribers mempty
-
-addSubscriber' :: Subscription -> Subscribers -> Subscribers
-addSubscriber' (Subscription textId eventType act) (Subscribers subscribers)
-    = Subscribers (M.alter upd eventType subscribers)
-    where
-        upd (Just a) = Just (M.insert textId act a)
-        upd _        = Just (M.singleton textId act)
-
-deleteSubscriber' :: Unsubscribe -> Subscribers -> Subscribers
-deleteSubscriber' (Unsubscribe textId eventType) (Subscribers subscribers)
-    = Subscribers (M.alter upd eventType subscribers)
-    where
-        upd (Just a) = Just (M.delete textId a)
-        upd _        = Nothing
-
-{-
-subscriptioService :: ActorL (IORef Subscribers)
-subscriptioService = do
--}
-
-postmanExample :: IO ()
-postmanExample = pure ()
-
 
 data Subscription = Subscription TextId EventType Notify
 data Unsubscribe  = Unsubscribe  TextId EventType
 
-{-
-makeDistribution :: Typeable a => [Subscription a] -> ActorL ()
-makeDistribution subscribers = do
-    subscribers' <- liftIO $ newIORef subscribers
-    math $ acceptSubscription subscribers'
-    math $ resending subscribers'
+multicast :: Typeable msg => IORef Subscribers -> msg -> IO ()
+multicast subscribers msg = do
+    broadcastList <- readIORef subscribers
+    multicast' broadcastList 
+    where
+        multicast' :: Subscribers -> IO ()
+        multicast' (Subscribers subscribers') =
+            whenJust (rawDataToType msg `M.lookup` subscribers') $
+                \broadcastList -> forM_ (M.elems broadcastList) $
+                    \(Notify dyn) -> whenJust (fromDynamic dyn) ($ msg)
 
-    acceptSubscription :: IORef [Subscription a] -> Subscription a -> IO ()
-    acceptSubscription ref newSubs = modifyIORef ref (newSubs:)
--}
+subscriptioService :: ActorL (IORef Subscribers)
+subscriptioService = do
+    subscribers <- liftIO $ newIORef emptySubscribers
+    math $ subsription subscribers
+    math $ unsubscribe subscribers
+    pure subscribers
+    where
+        emptySubscribers :: Subscribers
+        emptySubscribers = Subscribers mempty
 
-{-
+        subsription :: IORef Subscribers -> Subscription -> IO ()
+        subsription subscribers subs = modifyIORef' subscribers (addSubscriber subs)
+            where
+                addSubscriber :: Subscription -> Subscribers -> Subscribers
+                addSubscriber (Subscription textId eventType act) (Subscribers subscribers')
+                    = Subscribers (M.alter upd eventType subscribers')
+                    where
+                        upd (Just a) = Just (M.insert textId act a)
+                        upd _        = Just (M.singleton textId act)
+
+        unsubscribe :: IORef Subscribers -> Unsubscribe -> IO ()
+        unsubscribe subscribers unsub = modifyIORef' subscribers (deleteSubscriber unsub)
+            where
+                deleteSubscriber :: Unsubscribe -> Subscribers -> Subscribers
+                deleteSubscriber (Unsubscribe textId eventType) (Subscribers subscribers')
+                    = Subscribers (M.alter upd eventType subscribers')
+                    where
+                        upd (Just a) = Just (M.delete textId a)
+                        upd _        = Nothing
+
+
 data Times  = Times
 data WSPost = WSPost
+
+runPostman :: IO Actor
+runPostman = runActor logOff $ do
+    subscribers <- subscriptioService
+    math $ \Times  -> multicast subscribers Times
+    math $ \WSPost -> multicast subscribers WSPost
+
+makeNotify :: (Listener a msg, HaveTextId a, Typeable msg) => a -> msg -> Subscription 
+makeNotify subs msg = Subscription (getTextId subs) (rawDataToType msg) (packNotify ((subs `notify`) :: msg -> IO ()))
 
 postmanExample :: IO ()
 postmanExample = do
@@ -72,34 +79,17 @@ postmanExample = do
 
     wsAccepted    <- newFlag
     subs1         <- runSubscriberWSPost wsAccepted
-    postman `notify` (Subscription (subs1 `notify`) :: Subscription WSPost)
+    postman `notify` makeNotify subs1 WSPost
 
     timesAccepted <- newFlag
     subs2         <- runSubscriberTimes timesAccepted
-    postman `notify` (Subscription (subs2 `notify`) :: Subscription Times)
+    postman `notify` makeNotify subs2 Times
 
     postman `notify` Times
     postman `notify` WSPost
 
     wait wsAccepted
     wait timesAccepted
-
-runPostman :: IO Actor
-runPostman = runActor logOff $ do
-    makeDistribution ([] :: [Subscription WSPost])
-    makeDistribution ([] :: [Subscription Times])
-    where
-        makeDistribution :: Typeable a => [Subscription a] -> ActorL ()
-        makeDistribution subscribers = do
-            subscribers' <- liftIO $ newIORef subscribers
-            math $ acceptSubscription subscribers'
-            math $ resending subscribers'
-
-
-        resending :: IORef [Subscription a] -> a -> IO ()
-        resending ref msg = do
-            subscribers' <- readIORef ref
-            forM_ subscribers' $ \(Subscription s) -> s msg
 
 runSubscriberWSPost :: Flag -> IO Actor
 runSubscriberWSPost flag = runActor logOff $
@@ -108,4 +98,3 @@ runSubscriberWSPost flag = runActor logOff $
 runSubscriberTimes :: Flag -> IO Actor
 runSubscriberTimes flag = runActor logOff $
     math $ \Times -> liftFlag flag
--}
