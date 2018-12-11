f6ae812e7b0fabf9f1f330dae6b42f1ab1f8cece
Dmitrij Pavluk <al.lastor@bk.ru>
2018-12-11 19:36:57 +0300

    clearing of stream controller

--- src/Control/Concurrent/Node/Network/Tcp.hs
+++ src/Control/Concurrent/Node/Network/Tcp.hs
@@ -17,41 +17,7 @@ import qualified Network.Socket as S
 import qualified Network        as S hiding (accept)
 import           Control.Concurrent.Service.Network as X
 
-makeStream :: Loger -> Handle -> Int -> IO Stream
-makeStream loger handler maxPSize = runFsm loger Opened $ do
-    toLog Info "Start of stream controller"
-    subscribers <- subscriptioService
-    myRef       <- this
-    liftIO $ readerWorker (B.hGetSome handler maxPSize) myRef
-    math $ \(Inbox message) ->
-        multicast subscribers $ Message (getTextId myRef) message
-    math $ \msg -> catchAny
-        (B.hPut handler msg)
-        (\_ -> notify myRef CommandClose)
-
-    closeLogic subscribers myRef $ H.hClose handler
-
-
-makeTcpClient
-    :: Loger -> S.HostName -> S.PortNumber -> Int -> IO Stream
-makeTcpClient loger host port maxPSize =
-    catchAny (do
-        handle <- makeTcpConnection host port
-        makeStream loger handle maxPSize
-    ) (\exception -> do
-        loger Warn $ "Fail of start tcp client: " <> show exception
-        runFsm loger Closed $ addFinalState Closed
-    )
-
-makeTcpConnection :: S.HostName -> S.PortNumber -> IO Handle
-makeTcpConnection host port = do
-    address    <- head <$> S.getAddrInfo Nothing (Just host) (Just $ show port)
-    sock       <- S.socket (S.addrFamily address) S.Stream S.defaultProtocol
-    S.connect sock $ S.addrAddress address
-    S.socketToHandle sock ReadWriteMode
-
 makeFsm "TcpServer" [[t|CommandClose|], [t|Subscription|], [t|Unsubscribe|]]
-
 makeTcpServer
     :: forall a.(
         HaveTextId a,
@@ -84,11 +50,8 @@ tcpServer loger centralActor listenSock maxPSize = runFsm loger Opened $ do
         whileM $ catchAny (do
             (clientSock, _) <- S.accept listenSock
             handler     <- S.socketToHandle clientSock ReadWriteMode
-            sController <- makeStream loger handler maxPSize
-            -- sending events          from        to
-            $(subscribe [t|IsClosed|]) sController centralActor
-            $(subscribe [t|Message|])  sController centralActor
-            notify centralActor $ NewConnect sController
+            stream <- makeStream loger handler maxPSize
+            subscribeStreem stream centralActor
             pure True
         ) (\exception -> do
             fsmLoger Error $ "Fail of connect accepting: " <> show exception
@@ -96,3 +59,35 @@ tcpServer loger centralActor listenSock maxPSize = runFsm loger Opened $ do
             pure False
         )
     closeLogic subscribers myRef $ S.close listenSock
+
+makeTcpClient
+    :: Loger -> S.HostName -> S.PortNumber -> Int -> IO Stream
+makeTcpClient loger host port maxPSize =
+    catchAny (do
+        handle <- makeTcpConnection host port
+        makeStream loger handle maxPSize
+    ) (\exception -> do
+        loger Warn $ "Fail of start tcp client: " <> show exception
+        runFsm loger Closed $ addFinalState Closed
+    )
+
+makeTcpConnection :: S.HostName -> S.PortNumber -> IO Handle
+makeTcpConnection host port = do
+    address    <- head <$> S.getAddrInfo Nothing (Just host) (Just $ show port)
+    sock       <- S.socket (S.addrFamily address) S.Stream S.defaultProtocol
+    S.connect sock $ S.addrAddress address
+    S.socketToHandle sock ReadWriteMode
+
+makeStream :: Loger -> Handle -> Int -> IO Stream
+makeStream loger handler maxPSize = runFsm loger Opened $ do
+    toLog Info "Start of stream controller"
+    subscribers <- subscriptioService
+    myRef       <- this
+    liftIO $ readerWorker (B.hGetSome handler maxPSize) myRef
+    math $ \(Inbox message) ->
+        multicast subscribers $ Message (getTextId myRef) message
+    math $ \msg -> catchAny
+        (B.hPut handler msg)
+        (\_ -> notify myRef CommandClose)
+
+    closeLogic subscribers myRef $ H.hClose handler
--- src/Control/Concurrent/Service/Network.hs
+++ src/Control/Concurrent/Service/Network.hs
@@ -6,6 +6,7 @@ module Control.Concurrent.Service.Network where
 
 import           Control.Concurrent.Prelude
 import           Control.Concurrent.Model
+import qualified Data.Map as M
 import           Control.Monad.Extra hiding (whenJust)
 import qualified Data.ByteString as B
 import           Control.Concurrent.Service.Subscription
@@ -14,7 +15,7 @@ newtype IsClosed  = IsClosed TextId
 data CommandClose = CommandClose
 data Message      = Message TextId ByteString 
 newtype Inbox     = Inbox ByteString
-
+data ConnectManager = ConnectManager 
 
 makeStates ["Opened", "Closed"]
 makeFsm "Stream" [[t|CommandClose|], [t|ByteString|], [t|Subscription|], [t|Unsubscribe|]]
@@ -35,7 +36,6 @@ readerWorker readerAction myRef = void $ forkIO $ whileM $ do
         else notify myRef CommandClose
     pure retryReading
 
-
 closeLogic
     :: (HaveTextId a, Acception Closed (IO b))
     => IORef Subscribers -> a -> IO b -> StateMachineL ()
@@ -46,5 +46,24 @@ closeLogic subscribers myRef ioAction = do
         multicast subscribers $ IsClosed $ getTextId myRef
         ioAction
 
+subscribeStreem
+    :: (Listener a NewConnect
+    , Listener a Message
+    , Listener a IsClosed
+    , HaveTextId a)
+    => Stream -> a -> IO ()
+subscribeStreem stream centralActor = do
+    -- sending events          from   to
+    $(subscribe [t|IsClosed|]) stream centralActor
+    $(subscribe [t|Message|])  stream centralActor
+    notify centralActor $ NewConnect stream
 
---
+connectManager :: StateMachineL (M.Map TextId Stream)
+connectManager = do
+    toLog Info "Init connect manager service"
+    conncets <- liftIO $ newIORef mempty 
+    mathS ConnectManager $ \(NewConnect fsm) ->
+        void $ modifyIORef' conncets (M.insert (getTextId fsm) fsm)
+    mathS ConnectManager $ \(IsClosed textId) ->
+        void $ modifyIORef' conncets (M.delete textId)
+    pure conncets
